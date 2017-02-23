f9e73077cb207937114e22cce09c136dd126aa65
alejandroharb <alejandroharb@gmail.com>
2017-02-23 13:46:52 -0600

    added views directory for handlebars, set up public directory for static files, set up server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+//import npm dependencies
+var express = require('express');
+var path = require('path');
+var bodyParser = require("body-parser");
+var app = express();
+
+//======database connection, models=========
+var db = require('./models');
+
+//=========Handlebars Setup============
+var exphbr = require('express-handlebars');
+app.engine("handlebars", exphbr({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//server port
+var PORT = process.env.PORT || 3000;
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("./public"));
+
+db.sequelize.sync({force: true}).then(function() {
+    app.listen(PORT, function() {
+        console.log("listening on Port: " + PORT);
+    })
+})
