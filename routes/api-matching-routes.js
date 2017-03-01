59882cec1e0771093e64b39f2d746cc6871b95d8
alejandro harb <alejandro harb>
2017-02-28 22:25:34 -0600

    included google-distance npm

--- routes/api-matching-routes.js
+++ routes/api-matching-routes.js
@@ -1,17 +1,50 @@
 var db = require('../models');
+var geocoder = require('geocoder');
+var distance = require('google-distance');
 
 module.exports = function (app) {
     //==========USER MATCHING==========
     app.get('/match/:skill/:user', function (req, res) {
         var skill = req.params.skill;
         var username = req.params.user;
-        console.log("made it into route")
-        db.Golf.findAll({
-            where: { city: "Humble" },
-            include: [db.User]
-        }).then(function (data) {
-            ////do matching work here!!!!
-            res.json(data);
+        db.User.findOne({where:{user_name: username}})
+        .then(function(data) {
+            var address = data.dataValues.address
+            //use geocode function with callback to find city synchronously
+            findCity(address, function() {
+                db.Golf.findAll({
+                where: { city: city },
+                include: [{
+                    model: db.Golf,
+                    // where: {experience_rating: }
+                }]
+                }).then(function (data) {
+                    ////do matching work here!!!!
+
+                    res.json(data);
+                })
+            })
+
+            
         })
+        
     })
 }
+function findCity(loc, cb) {
+    geocoder.geocode(loc, function (err, data) {
+        console.log(data);
+        var locData = data.results[0].address_components;
+        for (var i = 0; i < locData.length; i++) {
+            if (locData[i].types[0] === "locality") {
+                city = locData[i].long_name;
+                console.log("=====city variable======")
+                console.log(city)
+                cb()
+                break;
+            }
+        }
+
+    })
+}
+
+findCity('3918 atascocita rd', function(){})
